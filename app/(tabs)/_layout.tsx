84b407b265ee225a11a5a2fdf945c2dfa2739657
marktipton <marktipton95@gmail.com>
2024-10-14 15:29:39 -0500

    remove explore tab and add profile to tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -24,11 +24,11 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="profile"
         options={{
-          title: 'Explore',
+          title: 'My Profile',
           tabBarIcon: ({ color, focused }) => (
-            <TabBarIcon name={focused ? 'code-slash' : 'code-slash-outline'} color={color} />
+            <TabBarIcon name={focused ? 'person-circle' : 'person-circle-outline'} color={color} />
           ),
         }}
       />
--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native"
 
 export default function Page() {
   return (
-    <View>
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Profile</Text>
     </View>
   )
