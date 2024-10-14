792e04e640b97a994c46a9c8de828720df5e0dfe
marktipton <marktipton95@gmail.com>
2024-10-14 15:59:18 -0500

    added existing account link in register page

--- app/register.tsx
+++ app/register.tsx
@@ -1,9 +1,14 @@
 import { Text, View } from "react-native"
+import { Link } from "expo-router"
 
 export default function Page() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Register</Text>
+      <Link href='/login'>
+        <Text>Log in to existing account</Text>
+      </Link>
     </View>
+
   )
 }
