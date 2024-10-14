b4c1914e3668818bbf36f671e50902046af4731b
marktipton <marktipton95@gmail.com>
2024-10-14 15:56:45 -0500

    remove headers from login and register and create link to register on login screen

--- app/login.tsx
+++ app/login.tsx
@@ -1,9 +1,13 @@
 import { Text, View } from "react-native"
+import { Link } from "expo-router"
 
 export default function Page() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Login</Text>
+      <Link href='/register'>
+        <Text>Create a new account</Text>
+      </Link>
     </View>
   )
 }
