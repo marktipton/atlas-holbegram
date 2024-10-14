dbf364720dc1e7753ceee3bbef9640ffb4042ac3
marktipton <marktipton95@gmail.com>
2024-10-14 15:36:38 -0500

    added favorites tab:

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -42,6 +42,15 @@ export default function TabLayout() {
         }}
       />
       <Tabs.Screen
+        name="favorites"
+        options={{
+          title: 'Favorites',
+          tabBarIcon: ({ color, focused }) => (
+            <TabBarIcon name={focused ? 'heart' : 'heart-outline'} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
         name="profile"
         options={{
           title: 'My Profile',
