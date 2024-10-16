ea29fd398dd42e7fea1bd1c8f2e27170a906585e
marktipton <marktipton95@gmail.com>
2024-10-16 12:16:40 -0500

    added expo image picker to image picker hook. button now opening photo library

--- hooks/useImagePicker.tsx
+++ hooks/useImagePicker.tsx
@@ -1,10 +1,27 @@
+import { usePermissions } from "expo-media-library";
 import { useState } from "react";
+import * as ImagePicker from 'expo-image-picker';
 
 export function useImagePicker(){
-  const [image, setImage] = useState(undefined);
+  const [image, setImage] = useState<string | undefined>(undefined);
+  const [status, requestPermission] = usePermissions();
 
-  function openImagePicker(){
-    alert("Open Image Picker");
+  async function openImagePicker(){
+    if(status === null){
+      const permission = await requestPermission();
+      if(permission.granted === false){
+        alert("You need to grant permission to access your images");
+        return;
+      }
+    }
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+    })
+
+    if(!result.canceled){
+      setImage(result.assets[0].uri);
+    }
   }
 
   function reset() {
