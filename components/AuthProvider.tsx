a9a3c318f4786c595d4e117891fb109a778e22c9
marktipton <marktipton95@gmail.com>
2024-10-16 16:50:02 -0500

    added login function to authprovider context

--- components/AuthProvider.tsx
+++ components/AuthProvider.tsx
@@ -1,12 +1,13 @@
 import { auth } from "@/firebaseConfig";
-import { createUserWithEmailAndPassword, UserCredential } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, UserCredential } from "firebase/auth";
 import { createContext, ReactNode, useContext } from "react";
 
-const AuthContext = createContext({register, logout});
+const AuthContext = createContext({register, logout, login});
 
 type AuthContextType = {
   register: (email: string, password: string) => Promise<UserCredential>;
   logout: () => Promise<void>;
+  login: (email: string, password: string) => Promise<UserCredential>;
 }
 
 export const useAuth = () => useContext<AuthContextType>(AuthContext)
@@ -19,8 +20,12 @@ function logout() {
   return auth.signOut();
 }
 
+function login(email: string, password: string) {
+  return signInWithEmailAndPassword(auth, email, password);
+}
+
 export function AuthProvider({children}: {children: ReactNode}){
-  return <AuthContext.Provider value={{register, logout}}>
+  return <AuthContext.Provider value={{register, logout, login}}>
     {children}
   </AuthContext.Provider>
 }
