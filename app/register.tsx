ce8483517eef4f734950d3f5e5e9d7f5e9eb2523
marktipton <marktipton95@gmail.com>
2024-10-16 16:36:20 -0500

    moved useState hooks to register...Email and password register succesfully routed and in Firebase!

--- app/register.tsx
+++ app/register.tsx
@@ -5,25 +5,24 @@ import { useState } from "react";
 import AuthForm from "@/components/AuthForm";
 
 export default function Page() {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
   const auth = useAuth();
   const router = useRouter();
 
-  async function register() {
+  async function register(email: string, password: string) {
     // alert(`Creating account with ${email} and ${password}`);
     try {
       await auth.register(email, password);
       router.replace("/(tabs)/");
     } catch (e) {
+      console.error(e);
       alert("Unable to create account");
     }
   }
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Register</Text>
-      <AuthForm onSubmit={register} buttonTitle="Sign Up"/>
+      <AuthForm onSubmit={register} buttonTitle="Create Account"/>
       <Link href='/login' replace>
         <Text>Log in to existing account</Text>
       </Link>
