a97989f118a7cc50aeb2e174f973df2f2ca4e0a9
marktipton <marktipton95@gmail.com>
2024-10-16 16:56:15 -0500

    log in to existing account is now working

--- app/login.tsx
+++ app/login.tsx
@@ -1,11 +1,24 @@
 import { Pressable, Text, View } from "react-native"
 import { Link, useRouter } from "expo-router"
+import { useAuth } from "@/components/AuthProvider";
+import AuthForm from "@/components/AuthForm";
 
 export default function Page() {
+  const auth = useAuth();
   const router = useRouter();
+
+  async function login(email: string, password: string) {
+    try {
+      await auth.login(email, password)
+      router.replace("/(tabs)/");
+    } catch (e) {
+      alert("Email or password is incorrect");
+    }
+  }
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Login</Text>
+      <AuthForm onSubmit={login} buttonTitle="Sign In"/>
       <Link href='/register' replace>
         <Text>Create a new account</Text>
       </Link>
