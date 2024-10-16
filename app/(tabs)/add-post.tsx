eb4e6469f54112490dad6394bb4c3ce60f5cc709
marktipton <marktipton95@gmail.com>
2024-10-16 11:47:58 -0500

    fixed href typing issue in external link

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,6 +1,9 @@
 import { Text, View } from "react-native"
 
 export default function Page() {
+  // const [caption, setCaption] = useState<string>("");
+  // const [loading, setLoading] = useState(false);
+  // const image = undefined;
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Add Post</Text>
--- components/ExternalLink.tsx
+++ components/ExternalLink.tsx
@@ -2,6 +2,7 @@ import { Link } from 'expo-router';
 import { openBrowserAsync } from 'expo-web-browser';
 import { type ComponentProps } from 'react';
 import { Platform } from 'react-native';
+import { Href } from 'expo-router';
 
 type Props = Omit<ComponentProps<typeof Link>, 'href'> & { href: string };
 
@@ -10,7 +11,7 @@ export function ExternalLink({ href, ...rest }: Props) {
     <Link
       target="_blank"
       {...rest}
-      href={href}
+      href={href as Href<string>}
       onPress={async (event) => {
         if (Platform.OS !== 'web') {
           // Prevent the default behavior of linking to the default browser on native.
