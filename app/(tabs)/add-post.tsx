be46277047f253f5b942ab244f8f1b386edae797
marktipton <marktipton95@gmail.com>
2024-10-16 12:04:30 -0500

    image picker hook alert now showing up on press in addpost page

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,12 +1,20 @@
-import { Text, View } from "react-native"
+import { useImagePicker } from "@/hooks/useImagePicker"
+import { Text, View, Button } from "react-native"
+
 
 export default function Page() {
   // const [caption, setCaption] = useState<string>("");
   // const [loading, setLoading] = useState(false);
   // const image = undefined;
+  const {image, openImagePicker, reset} = useImagePicker();
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center"}}>
       <Text>Add Post</Text>
+      <Button
+        title="Choose a photo"
+        // text="Choose a photo"
+        onPress={openImagePicker}
+      />
     </View>
   )
 }
