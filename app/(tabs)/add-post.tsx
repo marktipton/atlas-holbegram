6b773d5a04eaa8a865449178a30f204accb20fb9
marktipton <marktipton95@gmail.com>
2024-10-23 11:40:21 -0500

    added photo icon to buttona and teal color

--- app/(tabs)/add-post.tsx
+++ app/(tabs)/add-post.tsx
@@ -1,6 +1,9 @@
 import { useImagePicker } from "@/hooks/useImagePicker"
-import { Text, View, Button, Image } from "react-native"
+import { Text, View, Image, StyleSheet } from "react-native"
 import ImagePreview from "@/components/ImagePreview";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import { Colors } from "@/assets/colors/colors";
+import { Ionicons } from "@expo/vector-icons";
 
 
 export default function Page() {
@@ -15,11 +18,25 @@ export default function Page() {
         : <ImagePreview/>
       }
       <Text>Add Post</Text>
-      <Button
-        title="Choose a photo"
-        // text="Choose a photo"
-        onPress={openImagePicker}
-      />
+      <TouchableOpacity style={styles.button} onPress={openImagePicker}>
+        <Ionicons name="image-outline" size={24} color="white"/>
+        <Text style={styles.buttonText}>Choose a photo</Text>
+      </TouchableOpacity>
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: Colors.teal,
+    padding: 10,
+    borderRadius: 4,
+  },
+  buttonText: {
+    color: "white",
+    marginLeft: 8,
+    fontSize: 16,
+  },
+});
